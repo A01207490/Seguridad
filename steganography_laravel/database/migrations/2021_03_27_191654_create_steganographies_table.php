8728926a3fc2160b462ecca253b51ea5d46de0f7
Alberto <A01207490@itesm.mx>
2021-03-27 13:26:10 -0600

    Database setup

---
+++ steganography_laravel/database/migrations/2021_03_27_191654_create_steganographies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSteganographiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('steganographies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('steganography_key');
+            $table->string('steganography_message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('steganographies');
+    }
+}
